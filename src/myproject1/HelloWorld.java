76933f70295cffd8c8fe62144dc3b78d98029451
yukoga <yukoga@gmail.com>
2017-09-02 14:08:58 +0900

    merged.

--- src/myproject1/HelloWorld.java
+++ src/myproject1/HelloWorld.java
@@ -2,7 +2,7 @@ package myproject1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-    System.out.print("Practice git.");
+		System.out.print("Practice git.");
 		System.out.print("Hello World!");
 		System.out.print("日本語を追加！");
 		System.out.print("ほげほげ");

b6f6b7effe07f3ed1363b66f653ccfa15eccb324
yukoga <yukoga@gmail.com>
2017-09-02 13:57:38 +0900

    merge.

--- src/myproject1/HelloWorld.java
+++ src/myproject1/HelloWorld.java
@@ -1,9 +1,7 @@
 package myproject1;
 
 public class HelloWorld {
-	// test method
 	public static void main(String[] args) {
-		// add additional comment.
 		System.out.print("Hello World!");
 		System.out.print("日本語を追加！");
 	}

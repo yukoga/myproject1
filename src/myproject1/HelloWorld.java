1c86683b8e8ac993bf5fa6047a0ff00803358e89
yukoga <yukoga@gmail.com>
2017-09-02 14:01:21 +0900

    add here is additional comment.

--- src/myproject1/HelloWorld.java
+++ src/myproject1/HelloWorld.java
@@ -3,6 +3,7 @@ package myproject1;
 // add additional comments.
 public class HelloWorld {
 	public static void main(String[] args) {
+		// here is additional comment.
 		System.out.print("Hello World!");
 		System.out.print("日本語を追加！");
 	}

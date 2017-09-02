f34092859b150a1d9fc993cdf8570bc638bb82ce
yukoga <yukoga@gmail.com>
2017-09-02 13:40:21 +0900

    Merge from remote.

--- src/myproject1/HelloWorld.java
+++ src/myproject1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World!");
+		System.out.print("日本語を追加！");
 	}
 
 }

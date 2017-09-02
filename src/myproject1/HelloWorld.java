1574fb2243bf6e647310d3e58d6be842fa2f570b
yukoga <yukoga@gmail.com>
2017-09-02 13:42:29 +0900

    remove comments.

--- src/myproject1/HelloWorld.java
+++ src/myproject1/HelloWorld.java
@@ -1,11 +1,8 @@
 package myproject1;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.print("Hello World!");
 		System.out.print("日本語を追加！");
 	}
-
 }
